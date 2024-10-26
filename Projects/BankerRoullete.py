7dcf0deca98f4c14ac5fc1e27118e79c22cd074c
Ayush Mishra <130413472+AyushMishra1603@users.noreply.github.com>
2024-10-26 06:01:26 +0530

    Update BankerRoullete.py

--- Projects/BankerRoullete.py
+++ Projects/BankerRoullete.py
@@ -6,8 +6,10 @@ Name = input("Please enter the name of your friends!\n")
 #Placing the names into indexes of List
 Name_List = Name.split(",")
 ListLength = len(Name_List)
+
 #Pick a random index number and store it in a variable
 Randomchoice = random.randint(0, ListLength - 1)
+
 #Assign random name into new variable
 Billpayer = Name_List[Randomchoice]
 
